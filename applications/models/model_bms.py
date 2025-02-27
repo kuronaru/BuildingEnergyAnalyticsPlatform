eaa2ac8502b1b8ffd131621504dfdaeacbc91e12
T3Nto10 <tenderhjy@gmail.com>
2025-02-27 14:40:43 +0800

    fix: 更新requirements,修改时间为新加坡时区

--- applications/models/model_bms.py
+++ applications/models/model_bms.py
@@ -1,5 +1,6 @@
 from applications.extensions import db
-
+from datetime import datetime
+from pytz import timezone
 
 class BMSData(db.Model):
     """存储 BACnet 读取的数据"""
@@ -10,7 +11,7 @@ class BMSData(db.Model):
     object_type = db.Column(db.String(50), nullable=False)
     object_instance = db.Column(db.Integer, nullable=False)
     value = db.Column(db.Float, nullable=False)
-    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
+    timestamp = db.Column(db.DateTime, default=lambda : datetime.now(timezone('Asia/Singapore')))
 
 class BMSModel():
     @staticmethod
